859c7f398073474068b4693bbfb81c586e04e2f0
prakhar <prakhar_p@outlook.com>
2026-05-13 18:23:48 +0530

    updated main

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ import threading
 import uvicorn
 
 from app.communication.gmail import start_email_listener
-from app.services.scheduler import scheduler
 from app.services.scheduler import schedule_daily_report
 
 schedule_daily_report("918924913047")
@@ -18,6 +17,7 @@ if __name__ == "__main__":
     gmail_thread = threading.Thread(
         target=run_gmail,
         daemon=True,
+        name="GmailListenerThread",
     )
 
     gmail_thread.start()
